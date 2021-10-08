b701b417c6ff017638792be88e114a9adfda2d13
Sahith-Nagadesi <79249010+Sahith-Nagadesi@users.noreply.github.com>
2021-10-08 22:23:54 +0530

    Website

--- docs/javascript_files/portfolio.js
+++
@@ -1,9 +0,0 @@
-function enhance(id) {
-    document.getElementById(id).style.backgroundColor="rgba(49, 224, 224, 0.8)";
-    document.getElementById(id).style.color="#FF0000";
-}
-
-function regain(id) {
-    document.getElementById(id).style.backgroundColor="#FFFFFF";
-    document.getElementById(id).style.color="#0d5555";
-}
