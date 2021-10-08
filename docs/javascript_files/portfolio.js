463bcb8e4d56ff1a1112b175eb36f7e83a43ba6d
Sahith-Nagadesi <79249010+Sahith-Nagadesi@users.noreply.github.com>
2021-10-08 21:18:57 +0530

    Website

---
+++ docs/javascript_files/portfolio.js
@@ -0,0 +1,9 @@
+function enhance(id) {
+    document.getElementById(id).style.backgroundColor="rgba(49, 224, 224, 0.8)";
+    document.getElementById(id).style.color="#FF0000";
+}
+
+function regain(id) {
+    document.getElementById(id).style.backgroundColor="#FFFFFF";
+    document.getElementById(id).style.color="#0d5555";
+}
